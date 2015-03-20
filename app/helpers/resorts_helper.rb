feed1cc75ce472d29765ba0df746f5a2c7a4e33c
Justin <justinmckelvey@hotmail.com>
2015-03-20 13:07:13 -0500

    helper fix

--- app/helpers/resorts_helper.rb
+++ app/helpers/resorts_helper.rb
@@ -1,6 +1,6 @@
 module ResortsHelper
 
-  def flights_for_resort(flights:, resort:)
+  def flights_for_resort(flights: resort:)
     flights.select do |flight|
       resort.cities.include? flight.destination
     end
