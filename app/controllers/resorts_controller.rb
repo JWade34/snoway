c47cebd3cbaf7f228f277c69ede5753cdfe942ca
Justin <justinmckelvey@hotmail.com>
2015-03-25 14:05:01 -0500

    flight links

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -2,20 +2,38 @@ class ResortsController < ApplicationController
 
   def index
     @flight = Flight.all
+    date = Date.today + 1
+    @day = date.strftime("%Y-%m-%d")
+
+    week = Date.today + 7
+    @add_week = week.strftime("%Y-%m-%d")
+
 
     if params[:search_text].present?
       @resorts = Resort.search(params[:search_text])
     else
-      @resorts = Resort.all.sort_by(&:expected_snowfall).reverse
+      @resorts = Resort.all.sort_by(&:expected_snowfall).reverse.first(12)
     end
   end
 
   def show
+    date = Date.today + 1
+    @day = date.strftime("%Y-%m-%d")
+
+    week = Date.today + 7
+    @add_week = week.strftime("%Y-%m-%d")
+
     @resort = Resort.find params[:id]
     @flight = Flight.all
   end
 
   def flight
+    date = Date.today + 1
+    @day = date.strftime("%Y-%m-%d")
+
+    week = Date.today + 7
+    @add_week = week.strftime("%Y-%m-%d")
+
     @flight = Flight.all
   end
 
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,21 @@
 class Flight < ActiveRecord::Base
+
+  def resort
+    if destination == "Denver"
+      "Denver"
+    elsif destination == "Salt Lake"
+      "Park City"
+    end
+  end
+
+
+  def airport
+    if destination == "Denver"
+      "DEN"
+    else
+      ""
+    end
+
+  end
+
 end
